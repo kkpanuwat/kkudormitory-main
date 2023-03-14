870e0bb9f64d2798fb4d3c427655113cd57783f6
kkpanuwat <panuwat20481@gmail.com>
2023-03-15 03:55:50 +0700

    update fun search

--- src/main/java/com/kkudormitory/kkudormitory/controller/SearchApi.java
+++ src/main/java/com/kkudormitory/kkudormitory/controller/SearchApi.java
@@ -1,18 +1,20 @@
 package com.kkudormitory.kkudormitory.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.sql.*;
 import java.util.*;
-import com.kkudormitory.kkudormitory.model.repository.SearchRepo;
-
-import org.springframework.web.bind.annotation.PathVariable;
+import com.kkudormitory.kkudormitory.model.repository.*;
 
 @RestController
 @RequestMapping("api/search")
 public class SearchApi {
     
+    @Autowired
+	private SearchRepository repo;
+
     @GetMapping("/{search}")
-    public List<Map<String, Object>> getDormitory(@PathVariable("search") String search) throws SQLException, ClassNotFoundException {
-        SearchRepo se = new SearchRepo();
-        return se.searchDorm(search);
+    public List<Object> testT(@RequestParam("search") String search){
+        
+        return repo.searchDorm(search);
     }
 }
